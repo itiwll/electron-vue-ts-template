65fe4043fae9f6e122d8479fdada9c00adb6fbb6
Eson Wong <itiwll@gmail.com>
2020-07-10 15:07:51 +0800

    Init

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/*.{j,t}s?(x)',
       ],
       env: {
         mocha: true,
---
+++ tests/unit/electron.spec.js
@@ -0,0 +1,44 @@
+import testWithSpectron from 'vue-cli-plugin-electron-builder/lib/testWithSpectron';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+// eslint-disable-next-line no-undef
+const spectron = __non_webpack_require__('spectron');
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('Application launch', function applicationLaunch() {
+  this.timeout(30000);
+
+  beforeEach(function beforeEach() {
+    return testWithSpectron(spectron).then((instance) => {
+      this.app = instance.app;
+      this.stopServe = instance.stopServe;
+    });
+  });
+
+  beforeEach(function beforeEach() {
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness;
+  });
+
+  afterEach(function afterEach() {
+    if (this.app && this.app.isRunning()) {
+      return this.stopServe();
+    }
+    return undefined;
+  });
+
+  it('opens a window', function openWindow() {
+    return this.app.client
+      .getWindowCount()
+      .should.eventually.have.at.least(1)
+      .browserWindow.isMinimized()
+      .should.eventually.be.false.browserWindow.isVisible()
+      .should.eventually.be.true.browserWindow.getBounds()
+      .should.eventually.have.property('width')
+      .and.be.above(0)
+      .browserWindow.getBounds()
+      .should.eventually.have.property('height')
+      .and.be.above(0);
+  });
+});
