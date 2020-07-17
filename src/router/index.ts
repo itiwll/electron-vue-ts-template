3f04919d8c251e4c6f40f728aa7ea9b6b58667ad
eson <itiwll@gmail.com>
2020-07-17 18:25:22 +0800

    Feature: 页面组件示例

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
+import example from './example';
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,7 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  example,
 ];
 
 const router = new VueRouter({
---
+++ src/views/Example.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="view-example">
+    <h2>示例页面组件</h2>
+    <p class>页面组件主要包括布局和子组件</p>
+    <code>123</code>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class Example extends Vue {
+
+}
+</script>
